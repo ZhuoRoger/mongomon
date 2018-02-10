7289f241842467e61ea2b8a80e6d0b475044d4a1
laoshancun <laoshancun@foxmail.com>
2018-02-10 17:36:38 +0800

    1. repair code format. 2. use 127.0.0.1 as mongodb host rather than hostname.

--- bin/mongodb_monitor.py
+++ bin/mongodb_monitor.py
@@ -19,51 +19,51 @@ ts = int(time.time())
 # all falcon counter type metrics list
 
 mongodb_counter_metric = ["asserts_msg",
-                "asserts_regular",
-                "asserts_rollovers",
-                "asserts_user",
-                "asserts_warning",
-                "page_faults",
-		"connections_totalCreated",
-                "locks_Global_acquireCount_ISlock",
-                "locks_Global_acquireCount_IXlock",
-                "locks_Global_acquireCount_Slock",
-                "locks_Global_acquireCount_Xlock",
-                "locks_Global_acquireWaitCount_ISlock",
-                "locks_Global_acquireWaitCount_IXlock",
-                "locks_Global_timeAcquiringMicros_ISlock",
-                "locks_Global_timeAcquiringMicros_IXlock",
-                "locks_Database_acquireCount_ISlock",
-                "locks_Database_acquireCount_IXlock",
-                "locks_Database_acquireCount_Slock",
-                "locks_Database_acquireCount_Xlock",
-                "locks_Collection_acquireCount_ISlock",
-                "locks_Collection_acquireCount_IXlock",
-                "locks_Collection_acquireCount_Xlock",
-                "opcounters_command",
-                "opcounters_insert",
-                "opcounters_delete",
-                "opcounters_update",
-                "opcounters_query",
-                "opcounters_getmore",
-                "opcountersRepl_command",
-                "opcountersRepl_insert",
-                "opcountersRepl_delete",
-                "opcountersRepl_update",
-                "opcountersRepl_query",
-                "opcountersRepl_getmore",
-                "network_bytesIn",
-                "network_bytesOut",
-                "network_numRequests",
-                "backgroundFlushing_flushes",
-                "backgroundFlushing_last_ms",
-                "cursor_timedOut",
-                "wt_cache_readinto_bytes",
-                "wt_cache_writtenfrom_bytes",
-                "wt_bm_bytes_read",
-                "wt_bm_bytes_written",
-                "wt_bm_blocks_read",
-                "wt_bm_blocks_written"]
+        "asserts_regular",
+        "asserts_rollovers",
+        "asserts_user",
+        "asserts_warning",
+        "page_faults",
+        "connections_totalCreated",
+        "locks_Global_acquireCount_ISlock",
+        "locks_Global_acquireCount_IXlock",
+        "locks_Global_acquireCount_Slock",
+        "locks_Global_acquireCount_Xlock",
+        "locks_Global_acquireWaitCount_ISlock",
+        "locks_Global_acquireWaitCount_IXlock",
+        "locks_Global_timeAcquiringMicros_ISlock",
+        "locks_Global_timeAcquiringMicros_IXlock",
+        "locks_Database_acquireCount_ISlock",
+        "locks_Database_acquireCount_IXlock",
+        "locks_Database_acquireCount_Slock",
+        "locks_Database_acquireCount_Xlock",
+        "locks_Collection_acquireCount_ISlock",
+        "locks_Collection_acquireCount_IXlock",
+        "locks_Collection_acquireCount_Xlock",
+        "opcounters_command",
+        "opcounters_insert",
+        "opcounters_delete",
+        "opcounters_update",
+        "opcounters_query",
+        "opcounters_getmore",
+        "opcountersRepl_command",
+        "opcountersRepl_insert",
+        "opcountersRepl_delete",
+        "opcountersRepl_update",
+        "opcountersRepl_query",
+        "opcountersRepl_getmore",
+        "network_bytesIn",
+        "network_bytesOut",
+        "network_numRequests",
+        "backgroundFlushing_flushes",
+        "backgroundFlushing_last_ms",
+        "cursor_timedOut",
+        "wt_cache_readinto_bytes",
+        "wt_cache_writtenfrom_bytes",
+        "wt_bm_bytes_read",
+        "wt_bm_bytes_written",
+        "wt_bm_blocks_read",
+        "wt_bm_blocks_written"]
 
 
 mongodb_hostname = socket.gethostname()
@@ -75,28 +75,29 @@ mongodb_items = y["items"]
 
 for mongodb_ins in mongodb_items:
 
-        mongodb_monitor = mongodbMonitor()
-	
-        mongodb_tag = "mongo=" + str(mongodb_ins["port"])
+    mongodb_monitor = mongodbMonitor()
 
-        err,conn = mongodb_monitor.mongodb_connect(host=mongodb_hostname,port=mongodb_ins["port"], user=mongodb_ins["user"], password=mongodb_ins["password"])
- 
-	mongodb_upate_list = [] 
-        if err != 0:
-		key_item_dict =  {"endpoint": mongodb_hostname, "metric": "mongo_local_alive", "tags":mongodb_tag , "timestamp":ts, "value": 0, "step": 60, "counterType": "GAUGE"}
-		mongodb_upate_list.append(key_item_dict)
-		r = requests.post(falcon_client,data=json.dumps(mongodb_upate_list))
-		continue   #The instance is dead. upload the "mongo_alive_local=0" key, then continue.
+    mongodb_tag = "mongo=" + str(mongodb_ins["port"])
 
-        mongodb_dict = mongodb_monitor.get_mongo_monitor_data(conn)
-        mongodb_dict_keys = mongodb_dict.keys()
-	
-        for mongodb_metric in mongodb_dict_keys:
+    err,conn = mongodb_monitor.mongodb_connect(host="127.0.0.1",port=mongodb_ins["port"], user=mongodb_ins["user"], password=mongodb_ins["password"])
 
-                if mongodb_metric in mongodb_counter_metric :
-                        key_item_dict = {"endpoint": mongodb_hostname, "metric": mongodb_metric, "tags":mongodb_tag , "timestamp":ts, "value": mongodb_dict[mongodb_metric], "step": 60, "counterType": "COUNTER"}
-                else:
-                        key_item_dict =  {"endpoint": mongodb_hostname, "metric": mongodb_metric, "tags":mongodb_tag , "timestamp":ts, "value": mongodb_dict[mongodb_metric], "step": 60, "counterType": "GAUGE"}
+    mongodb_upate_list = [] 
+    if err != 0:
+        key_item_dict =  {"endpoint": mongodb_hostname, "metric": "mongo_local_alive", "tags":mongodb_tag , "timestamp":ts, "value": 0, "step": 60, "counterType": "GAUGE"}
+        mongodb_upate_list.append(key_item_dict)
+        r = requests.post(falcon_client,data=json.dumps(mongodb_upate_list))
+        continue   #The instance is dead. upload the "mongo_alive_local=0" key, then continue.
 
-                mongodb_upate_list.append(key_item_dict)
-	r = requests.post(falcon_client,data=json.dumps(mongodb_upate_list))
+    mongodb_dict = mongodb_monitor.get_mongo_monitor_data(conn)
+    mongodb_dict_keys = mongodb_dict.keys()
+
+    for mongodb_metric in mongodb_dict_keys:
+
+        if mongodb_metric in mongodb_counter_metric :
+            key_item_dict = {"endpoint": mongodb_hostname, "metric": mongodb_metric, "tags":mongodb_tag , "timestamp":ts, "value": mongodb_dict[mongodb_metric], "step": 60, "counterType": "COUNTER"}
+        else:
+            key_item_dict =  {"endpoint": mongodb_hostname, "metric": mongodb_metric, "tags":mongodb_tag , "timestamp":ts, "value": mongodb_dict[mongodb_metric], "step": 60, "counterType": "GAUGE"}
+
+        mongodb_upate_list.append(key_item_dict)
+
+    r = requests.post(falcon_client,data=json.dumps(mongodb_upate_list))
